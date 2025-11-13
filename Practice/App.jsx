9b52f451c7831376bce6e45e5423d524f52a74bd
Abdullah AL Musabbir <108293569+abdullahalmusabbir@users.noreply.github.com>
2025-11-13 22:32:15 +0600

    Add grid of Card components to App
    Replaced the greeting with a grid of Card components.

--- Practice/App.jsx
+++ Practice/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 style={{ color: "white" }}>Hello Boss</h1>
+      <div className="grid grid-cols-3 gap-3 justify-center mt-10 items-center w-full h-100 bg-gray-200">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      
     </>
   )
 }
