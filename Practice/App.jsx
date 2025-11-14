78d083fdede7993d3b9cad74e47ff9e5c597a8bf
Abdullah AL Musabbir <108293569+abdullahalmusabbir@users.noreply.github.com>
2025-11-14 20:02:05 +0600

    Add multiple Card components and social icons

--- Practice/App.jsx
+++ Practice/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card.jsx'
+import STATE from './components/STATE.jsx'
+import Log from './components/Conditional_rendering/log.jsx'
+
+import { FaFacebook,FaYoutube } from "react-icons/fa";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,12 +14,15 @@ function App() {
   return (
     <>
       <div className="grid grid-cols-3 gap-3 justify-center mt-10 items-center w-full h-100 bg-gray-200">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card number="01944145084"/>
+        <Card number="01944145085"/>
+        <Card number="01944145086"/>
+        <Card number="01944145087"/>
+        <Card number="01944145088"/>
+        <Card number="01944145089"/>
+        <span><FaFacebook className="text-black text-center self-center"/></span>
+        <span><FaYoutube className="text-black text-center self-center"/></span>
+        <STATE />
       </div>
       
     </>
